f831caaeaea9185fe7dfc3a59cb2ee05816446f0
Lucas Tymen <lucas.tymen@gmail.com>
2023-03-04 10:59:08 +0100

    resolved

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ const order = {
 // Refactor the code below:
 
 checkInventory(order)
-    .then((resolvedValueArray) => {
-        processPayment(resolvedValueArray)
-            .then((resolvedValueArray) => {
-                shipOrder(resolvedValueArray)
-                    .then((successMessage) => {
-                        console.log(successMessage);
-                    });
-            });
-    });
+.then((resolvedValueArray) => {
+  return processPayment(resolvedValueArray);
+})
+.then((resolvedValueArray) => {
+  return shipOrder(resolvedValueArray);
+})
+.then((successMessage) => {
+  console.log(successMessage);
+});
